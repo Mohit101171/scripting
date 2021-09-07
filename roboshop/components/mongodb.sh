7611b83e4f7f70677b845d997f9b26c1f2d04550
EC2 Default User <ec2-user@ip-172-31-59-72.ec2.internal>
2021-09-07 19:29:27 +0000

    making changes to mongodb script

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -11,21 +11,21 @@ gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mong
  yum install -y mongodb-org 
 
 
-#sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-
-#Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
-#Config file: /etc/mongod.conf
-
-# systemctl enable mongod
-# systemctl start mongod
+ sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 
-# systemctl restart mongod
 
-# curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 
-# cd /tmp
-# unzip mongodb.zip
-# cd mongodb-main
-# mongo < catalogue.js
-# mongo < users.js 
+ systemctl enable mongod
+ systemctl start mongod
+
+
+ systemctl restart mongod
+
+ curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+
+ cd /tmp
+ unzip mongodb.zip
+ cd mongodb-main
+ mongo < catalogue.js
+ mongo < users.js 
