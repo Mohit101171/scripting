a8b3912d2a58670a2d4763e051af7a4164771c4a
Mohit101171 <mk.singh0246@gmail.com>
2021-09-12 08:32:39 +0000

    ok

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,42 +1,83 @@
 #!/bin/bash
 
-source components/common.sh
 
-Print "Setting up MongoDB Repo"
+Status_Check() {
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 2
+  fi
+} 
+
+
+echo "Setting up MongoDB Repo"
+
 echo '[mongodb-orog-4.2]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
-
 Status_Check $?
 
-Print "Installing MongoDB"
- yum install -y mongodb-org &>>$LOG
- 
+echo "Installing MongoDB"
+ yum install -y mongodb-org &>>tmp/log
+ if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 2
+  fi 
 
-Print "Configuring MongoDB"
+echo "Configuring MongoDB"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-Status_Check $1
+ if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 2
+  fi
 
-Print "Starting MongoDB"
+echo "Starting MongoDB"
  systemctl enable mongod
  systemctl restart mongod
-Status_Check $1
+ if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 2
+  fi 
+
 
 Print "Downloading MongoDB Schema"
  curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+ if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 2
+  fi
 
  cd /tmp
  Print "Extracting Schema"
- unzip mongodb.zip &>>$LOG
- Status_Check $1
- 
+ unzip mongodb.zip &>>tmp/log
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 2
+  fi
+
  
  cd mongodb-main
  Print "Loading Schema\t\t"
- mongo < catalogue.js &>>$LOG
- mongo < users.js  &>>$LOG
+ mongo < catalogue.js &>>tmp/log
+ mongo < users.js  &>>tmp/log
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 2
+  fi
  
- exit 0
