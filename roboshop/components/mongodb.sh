2d534b389e2fea8055ea0c3c641614c0622ee3dc
EC2 Default User <ec2-user@ip-172-31-59-72.ec2.internal>
2021-09-12 06:34:58 +0000

    Changes for mongodb

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,31 +1,52 @@
-echo "Installing MongoDB"
+#!/bin/bash
 
+Status_Check() {
+  if [$1 -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 2
+  fi
+} 
 
-echo '[mongodb-org-4.2]
+Print() {
+  echo -n -e "$1 \t- "
+}
+ 
+Print "Setting up MongoDB Repo"
+echo '[mongodb-orog-4.2]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+Status_Check $?
 
- yum install -y mongodb-org 
+Print "Installing MongoDB"
+ yum install -y mongodb-org &>>$LOG
+ 
 
-
+Print "Configuring MongoDB"
  sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+Status_Check $1
 
-
-
-
+Print "Starting MongoDB"
  systemctl enable mongod
- systemctl start mongod
-
-
  systemctl restart mongod
+Status_Check $1
 
+Print "Downloading MongoDB Schema"
  curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 
  cd /tmp
- unzip mongodb.zip
+ Print "Extracting Schema"
+ unzip mongodb.zip &>>$LOG
+ Status_Check $1
+ 
+ 
  cd mongodb-main
- mongo < catalogue.js
- mongo < users.js 
+ Print "Loading Schema\t\t"
+ mongo < catalogue.js &>>$LOG
+ mongo < users.js  &>>$LOG
+ 
+ exit 0
