0e493d1a9e5b31c3350c9b9e6b75456bc3203a77
Mohit101171 <mk.singh0246@gmail.com>
2021-09-12 15:42:10 +0000

    Making changes to mongodb script

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,2 +1,30 @@
-echo "Installing Catalogue"
+#!bin/bash
+
+source components/common.sh
+
+Print "Installing NodeJS"
+yum install nodejs make gcc-c++ -y &>>$LOG
+Status_Check $?
+
+Print "Adding Roboshop User"
+useradd robohsop &>>$LOG
+Status_Check $?
+
+Print "Downloading Catalogue Content"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+Status_Check $?
+
+Print "Extraxting Catalogue"
+cd /home/roboshop
+unzip /tmp/catalogue.zip &>>$LOG 
+mv catalogue-main catalogue
+Status_Check $?
+
+cd /home/roboshop/catalogue
+npm install &>>$LOG
+
+# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+# systemctl daemon-reload
+# systemctl start catalogue
+# systemctl enable catalogue
 
