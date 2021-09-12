b2c933f9c999139c48c65b3592999cc8f2a0cf5d
EC2 Default User <ec2-user@ip-172-31-59-72.ec2.internal>
2021-09-12 08:16:00 +0000

    ok

---
+++ roboshop/components/common.sh
@@ -0,0 +1,22 @@
+
+Status_Check() {
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 2
+  fi
+} 
+
+Print() {
+  echo -n -e "$1 \t- "
+}
+
+if [ $UID -ne 0 ]; then
+    echo -e "\e[1;33m You should exevute this script as root user\e[0m"
+    exit 1
+    
+fi
+
+LOG=/tmp/roboshop.log
+rm -f $LOG
