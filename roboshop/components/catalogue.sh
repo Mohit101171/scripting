b6441ad4375b6aa5d175884f747f22bafa5bbe84
Mohit101171 <mk.singh0246@gmail.com>
2021-09-14 06:26:00 +0000

    Pushing catalogue

---
+++ roboshop/components/catalogue.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source components/common.sh
+
+Print "Installing NodeJS"
+yum install nodejs make gcc-c++ -y &>>$LOG
+Status_Check $?
+
+Print "Adding Roboshop User"
+id roboshop &>>$LOG
+if [ $ -eq 0 ]; then
+echo "User already exists, hence skipping"
+else
+useradd roboshop &>>$LOG
+useradd robohsop &>>$LOG
+fi
+Status_Check $?
+
+Print "Downloading Catalogue Content"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+Status_Check $?
+
+Print "Extracting Catalogue"
+cd /home/roboshop
+unzip /tmp/catalogue.zip &>>$LOG 
+mv catalogue-main catalogue
+Status_Check $?
+
+cd /home/roboshop/catalogue
+npm install --unsafe-perm &>>$LOG
+
+# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+# systemctl daemon-reload
+# systemctl start catalogue
+# systemctl enable catalogue
+
