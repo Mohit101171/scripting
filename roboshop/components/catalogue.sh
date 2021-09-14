0a5c48b0ad1e224135085190c53ee024928436bd
Mohit101171 <mk.singh0246@gmail.com>
2021-09-14 17:44:21 +0000

    Pushing catalogue

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -32,7 +32,7 @@ Status_Check $?
 chown roboshop:roboshop -R /home/roboshop
 
 Print "Update SystemD Service\t"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service 
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.servic
 Status_Check $?
 
 Print "Setup SystemD Service\t"
