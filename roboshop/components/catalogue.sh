559167fe900e23424aaea920daf55dcf453480e8
Mohit101171 <mk.singh0246@gmail.com>
2021-09-14 09:25:04 +0000

    Pushing catalogue

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -9,7 +9,7 @@ Status_Check $?
 Print "Adding Roboshop User"
 id roboshop &>>$LOG
 if [ $? -eq 0 ]; then
-echo "User already exists, hence skipping\t"
+echo "User already exists, hence skipping \t"
 else
 useradd roboshop &>>$LOG
 fi
