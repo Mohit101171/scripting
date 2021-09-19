89eac6495d1edb9fc3b4b04996682f11220c86e9
Mohit101171 <mk.singh0246@gmail.com>
2021-09-19 10:30:53 +0000

    Updating User Script

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -5,3 +5,7 @@ source components/common.sh
 COMPONENT=user
 ## NODEJS is a function from common.sh
 NODEJS
+
+Print "Restart Nginx\t\t"
+systemctl restart nginx    &&  systemctl enable nginx
+Status_Check $?
