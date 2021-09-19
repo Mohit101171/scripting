d1ff1361c53ab44b83bba73a310f3f7b296cd744
Mohit101171 <mk.singh0246@gmail.com>
2021-09-19 09:08:42 +0000

    Updating frontend Script

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -23,5 +23,5 @@ sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/lo
 Status_Check $?
 
 Print "Restart Nginx\t\t"
-systemctl restat nginx    &&  systemctl enable nginx
+systemctl restart nginx    &&  systemctl enable nginx
 Status_Check $?
