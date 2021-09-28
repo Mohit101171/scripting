e8063827c6e80443ffee9508b7cb43d00ebd28c2
Mohit101171 <mk.singh0246@gmail.com>
2021-09-28 18:26:48 +0000

    ok

--- create-instance.sh
+++ create-instance.sh
@@ -21,7 +21,7 @@ if [ $? -eq 0 ]; then
   exit 0
 fi
 
-IP=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$INSTANCE_NAME}]", "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
+IP=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
 
 sed -e "s/INSTANCE_NAME/$INSTANCE_NAME/" -e "s/INSTANCE_IP/$IP/" record.json >/tmp/record.json
 aws route53 change-resource-record-sets --hosted-zone-id Z05154141PF035VH4D07A --change-batch file:///tmp/record.json | jq
